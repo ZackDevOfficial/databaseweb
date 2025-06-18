a65900ee0fb4151326593de09255c167256ff260
ZackDevOfficial <zenofc044@gmail.com>
2025-06-18 09:02:18 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -80,5 +80,5 @@ export default function Home() {
       <p>{status}</p>
     </main>
   );
-    }
-                                                                                                             
+      }
+    
